7f54c128df73a545c514a606cc9b1e972521d4c6
chirag <c.taneja09@gmail.com>
2024-09-08 21:35:36 +0530

    Shelter entity

--- adoption-system/src/main/java/com/chirag/adoption_system/model/Shelter.java
+++ adoption-system/src/main/java/com/chirag/adoption_system/model/Shelter.java
@@ -1,9 +1,17 @@
 package com.chirag.adoption_system.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "shelter")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Shelter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
