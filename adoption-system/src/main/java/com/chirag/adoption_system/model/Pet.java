aaf12e5f2955c1a31bda29dbe8b1aa5a96abfd67
chirag <c.taneja09@gmail.com>
2024-09-08 21:34:24 +0530

    pet entity

--- adoption-system/src/main/java/com/chirag/adoption_system/model/Pet.java
+++ adoption-system/src/main/java/com/chirag/adoption_system/model/Pet.java
@@ -1,9 +1,17 @@
 package com.chirag.adoption_system.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pet")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
